2b830d63411209d76fd2f66d091dea5f777029a6
Yusuf Yildiz <yusuf_yildiz@student.uml.edu>
2017-10-24 00:30:48 -0400

    Starting, Stopping, OnClick, & Beginning of UI Design
    Starting, Stopping, OnClick, & Beginning of UI Design

--- content.js
+++ content.js
@@ -1,11 +1,63 @@
-function Like() {
-    $(".recsGamepad .button:nth-child(4)").click()
-}
+$(function () {
+    var LikeBot = {
+        likeButton: ".recsGamepad .button:nth-child(4)",
+        discoverNewMatchesButton: ".discoverNewMatchesButton",
+        randFirst: 500,
+        randSecond: 500,
+        liking: false,
 
-function GetRandom() {
-    return Math.floor((Math.random() * 500) + 500);
-}
+        like: function() {
+            var ref = this;
+            this.likeIntervalId = setInterval(function() {
+                $(ref.likeButton).click();
+            }, this.getRandom());
+        },
+        getRandom: function() {
+            var ref = this;
+            return Math.floor((Math.random() * ref.randFirst) + ref.randSecond);
+        },
 
-$(document).ready(function() {
-    setInterval(function() {Like()}, GetRandom());
+        stop: function() {
+            var ref = this;
+            clearInterval(ref.likeIntervalId);
+        },
+
+        start: function() {
+            var ref = this;
+            var discoverBtnWait = setInterval(function() {
+                if($(ref.discoverNewMatchesButton).length) {
+                    $(ref.discoverNewMatchesButton).on("click", function(e) {
+                        e.preventDefault();
+                        ref.toggle();
+                    });
+                    ref.createSwipeButton();
+                    clearInterval(discoverBtnWait);
+                }
+            }, 100);
+        },
+
+        createSwipeButton: function() {
+            var discoverBtn = $(this.discoverNewMatchesButton);
+            var swipeBtn = discoverBtn.clone();
+            swipeBtn.find("h3").text("Begin Swiping");
+            swipeBtn.find(".Heading__subtitle").text("Click here to automate your swiping");
+            swipeBtn.insertBefore(discoverBtn);
+
+        },
+
+        toggle: function() {
+            if(this.liking) {
+                this.stop();
+                console.log("stopping");
+                this.liking = false;
+            } else {
+                this.like();
+                console.log("starting");
+                this.liking = true;
+            }
+        }
+    }
+
+    LikeBot.start();
+
 });
